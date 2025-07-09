2c2f5eb7bf82a874dd720ac1b3810b183982dfaa
rlfqls <wkd3641@naver.com>
2025-07-09 21:39:09 +0900

    고정길이 스택 구현

--- 4장/fixed_stack.py
+++ 4장/fixed_stack.py
@@ -36,4 +36,33 @@ class FixedStack:   # 고정 길이 스택 클래스
         return self.stk[self.ptr]
     
     def peek(self) -> Any:       # 스택에서 데이터를 피크(꼭대기 데이터를 들여다봄)
+        if self.is_empty():      # 스택이 비어있음
+            raise FixedStack.Empty  # 예외 처리 발생
+        return self.stk[self.ptr - 1]
+    
+    def clear(self) -> None:     # 스택을 비움(모든 데이터를 삭제)
+        self.ptr = 0
+
+    def find(self, value: Any) -> Any:  #스택에서 value를 찾아 인덱스를 반환(없으면 -1을 반환)
+        for i in range(self.ptr -1, -1, -1): # 꼭대기 쪽부터 선형 검색
+            if self.stk[i] == value:
+                return i           # 검색 성공
+            return -1              # 검색 실패
+        
+    def count(self, value: Any) -> int:  # 스택에 있는 value의 개수를 반환
+        c = 0
+        for i in range(self.ptr):  # 바닥 쪽부터 선형 검색
+            if self.stk[i] == value:  # 검색 성공
+                c += 1
+            return c
+        
+    def __contain__(self, value: Any) -> bool:   # 스택에 value가 있는지 판단
+        return self.count(value) > 0
+    
+    def dump(self) -> None:        # 덤프(스택 안에 모든 데이터를 바닥부터 꼭대기 순으로 출력)
+        if self.is_empty():        # 스택이 비어있음
+            print('스택이 비어 있습니다.')
+        else:
+            print(self.stk[:self.ptr])
+
 
